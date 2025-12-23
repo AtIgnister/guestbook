a704bdd6e02c62c4b9026ea07cd6ec1ae7e83d6e
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2025-12-23 20:46:54 +0100

    just realized we don't need that

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,10 @@
             @else
                 <a
                     href="{{ route('login') }}"
-                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm leading-normal"
+                    class="inline-block px-5 py-1.5 border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm leading-normal"
                 >
                     Log in
                 </a>
-
-                @if (Route::has('register'))
-                    <a
-                        href="{{ route('register') }}"
-                        class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm leading-normal">
-                        Register
-                    </a>
-                @endif
             @endauth
                 <a href="/privacy-policy" class="text-white mr-6 text-sm">Privacy Policy</a>
         </nav>
