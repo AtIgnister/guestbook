5226baf24b560c6af97d2781c5ce0a490f96795a
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2025-12-23 20:45:48 +0100

    fix register button

--- resources/views/partials/settings-heading.blade.php
+++ resources/views/partials/settings-heading.blade.php
@@ -1,4 +1,5 @@
 <div class="relative mb-6 w-full">
     <h1>{{ __('Settings') }}</h1>
     <p>{{ __('Manage your profile and account settings') }}</p>
+    <a href="{{ route("dashboard") }}">Return to dashboard</a>
 </div>
