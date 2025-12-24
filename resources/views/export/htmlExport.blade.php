da06ca3c58724b0a36493ee2bd19ddf150b157d4
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2025-12-24 22:12:07 +0100

    add more metadata to html export

--- resources/views/export/htmlExport.blade.php
+++ resources/views/export/htmlExport.blade.php
@@ -10,6 +10,8 @@
     <div class="entry-data">
         <p class="entry-author-name">{{ $entry->name }}</p>
         <p class="entry-guestbook-id">{{ $entry->guestbook_id }}</p>
+        <time class="entry-creation-date">{{ $entry->created_at }}</time>
+        <time class="entry-update-date">{{ $entry->updated_at }}</time>
         <a class="entry-author-website" href="{{ $entry->website }}">{{ $entry->website }}</a>
         <div class="entry-comment">{!! $entry->comment !!}</div>
     </div>
