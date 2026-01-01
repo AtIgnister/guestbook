073f1a33451e7840c7fb783419d6ddcaf58dbadd
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2026-01-01 03:51:08 +0100

    we don't need this menu option anymore

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,6 @@
     <h1>Dashboard</h1>
     <a class="block" href="{{ route('guestbooks.index')}}">View Guestbooks</a>
     <a class="block" href="{{ route('entries.editAll') }}">View all Entries</a>
-    <a class="block">Approve Entries</a>
     <a class="block" href="{{ route('guestbooks.export.index') }}">Export Data</a>
     <a class="block" href="{{ route('profile.edit') }}">Settings</a>
     <br>
