cb44bf6d1da0c184cd5861c1d28cefead38bb010
Tim <137562253+AtIgnister@users.noreply.github.com>
2026-01-17 20:34:08 +0100

    fix title

--- resources/views/users/banned.blade.php
+++ resources/views/users/banned.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Privacy Policy</title>
+    <title>Your account has been suspended.</title>
     @vite("resources/css/app.css")
 </head>
 <body>
