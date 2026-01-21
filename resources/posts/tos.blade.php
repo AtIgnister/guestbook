87910f83e012515f98c7c674ecb81eaec9959e62
Tim <137562253+AtIgnister@users.noreply.github.com>
2026-01-22 00:13:40 +0100

    TOS clarification part 1

---
+++ resources/posts/tos.blade.php
@@ -0,0 +1,84 @@
+<h1>Commenting Guidelines</h1>
+<p>Heya!</p>
+<p>Before you comment, here's some rules to follow to ensure guestbooks continues being an enjoyable website to use.</p>
+<br>
+<p>These rules apply site-wide, so no matter whose guestbook you're using, if you break one, I will delete your message. (and, depending on the infraction, IP ban you)</p>
+<br>
+<p>Here they are:</p>
+<ul>
+    <li>No racism/ableism/transphobia/homophobia etc.</li>
+    <li>Don't post anything that could get me in trouble. (links to pirated content, recordings of real-life violence, etc.)</li>
+    <li>No spam or phishing scams.</li>
+    <li>Don't encourage self-harm.</li>
+    <li>Keep things SFW - No explicit sexual content or graphic descriptions of gore.</li>
+    <li>Don't dox yourself/anyone else. I will delete your message if you share your phone number or adress.</li>
+</ul>
+<p>These obviously also go for the owners of the guestbooks. If you post anything on your own guestbook that violates these rules, I will either edit your message to remove the infringing content (if I see you accidentally doxxing yourself) or delete your account. (which will also automatically delete any guestbooks you're created)</p>
+<br>
+<p>I may add to these rules as we go, but in general: Just be reasonable. I try to interpret messages charitably. If you use guestbooks for their intended purpose of giving feedback or reaching out and initiating a conversation, you will never get your account banned.</p>
+<br>
+<p>I try to only act when it is very clear that someone is doing stuff that is blatantly against the rules. Guestbook owners have their own moderation tools, so if it won't get the site in trouble or is blatant hate speech, I'll leave it up to them.</p>
+
+<h2>Examples</h2>
+<p>In case you're still confused, here are some examples for which things do and don't fall under each respective rule:</p>
+<h3>No racism/ableism/transpobia/homophobia etc.</h3>
+<p>These are examples of what is not allowed:</p>
+<blockquote>"transgenders need to GTFO of the indie web"</blockquote>
+<blockquote>"stop shoving your LGBTQ bullshit down peoples throat"</blockquote>
+<blockquote>"stop complaining and go back to your country"</blockquote>
+<br>
+<p>These are an examples of what kind of speech is allowed:</p>
+<blockquote>"I saw your post about {insert character here} from {media franchise} actually being queer, and I don't think it makes sense narratively"</blockquote>
+<blockquote>"People that detransition are totally valid, the narrative around that has just sadly been overtaken by grifters that try to act like people are getting indoctrinated into being trans by the LGBTQ community"</blockquote>
+<br>
+<p>So, in short: If your comment involves insulting or disparaging people based on unrelated characteristics like race, sex or gender, you are not welcome.</p>
+<br>
+<p>If you do not do this, and simply disagree with others about their interpretation of certain media, or want to give a measured take on/expound upon certain queer topics, you are welcome to do so.</p>
+<br>
+<p>In general, enforcement of this rule is a bit more fuzzy, since there are a lot of dogwhistles that straddle that line between technically not being hateful while heavily implying things that would absolutely not be allowed under the spirit of the rules.</p>
+<br>
+<p>Generally, I'd like to be more cut-and-dry here than just 'be reasonable', but sadly here that's not entirely possible.</p>
+<br>
+<p>Though this is no place to start arguments anyways. We're a guestbook website, not a per-post comment section.</p>
+
+<h3>Don't post anything that could get me in trouble. (links to pirated content, recordings of real-life violence, etc.)</h3>
+<p>These are examples of what is not allowed:</p>
+<blockquote>I saw your post about the pricing of muse dash being unreasonable, you can actually pirate that game over at {insert link here}</blockquote>
+<br>
+<blockquote>It's wild seeing the kind of violence that happened on that stream. There's actually still an archived video here. {insert link}</blockquote>
+<br>
+<blockquote>I don't know why you blanked out the information: The dox of that user is all public info. You can just google {insert term here} to get their address.</blockquote>
+
+<br>
+<p>These are an examples of what kind of speech is allowed:</p>
+<blockquote>Honestly, I don't really agree with your stance on piracy. IMO, it's actually actively a good thing, morally</blockquote>
+<blockquote>It's insane how they let that stream stay up for so long. Somehow they still have archives of it up on the actual site.</blockquote>
+<br>
+<p>Basically, don't link or otherwise instruct people on how to get copyright infringing material, things that compromise the personal safety of private persons or footage of real-life violence.</p>
+<p>It's fine to express opinions on the topics covered by this rule. Just don't post links or instructions on how to access this stuff.</p>
+
+<h3>No spam or phishing scams.</h3>
+<p>These are examples of what is not allowed:</p>
+<blockquote>Your account has expired. Contact support over at guestbooks@sketchy-domain.local to prevent your guestbook from being deleted permanently.</blockquote>
+<blockquote>BILL GATES IS DOUBLING CRYPTO TO ANYONE WHO SENDS $ETH TO {insert wallet here}. ONLY 24 HOURS LEFT.</blockquote>
+<br>
+<p>These are an examples of what kind of speech is allowed:</p>
+<blockquote>For the longest time, someone was domain-squatting goggle.com and serving a malicious login page there. Wild stuff.</blockquote>
+<blockquote>NextJS got compromised recently. I saw you were using Next for your site, so you should probably check if you got affected.</blockquote>
+<blockquote>I've only had good experiences with g2a, I don't really get all the fearmongering.</blockquote>
+<br>
+<p>TLDR: You can talk about scams or malicious websites, as long as you make it clear that they are, in fact, malicious and that you should not visit them/participate.</p>
+<p>You should try not to share any direct links to these sites, but in cases where, i.e. the official name of the website and the domain is identical, it is still fine to name the site explicitly.</p>
+<p>Posts that solely exist to solicit money or personal information will be removed.</p>
+<br>
+<p>It is fine to talk about your positive experiences with websites or services that are generally perceived to provide a poor/inconsistent service.</p>
+<br>
+<p>If the websites primary purpose is taking your money/personal information and then giving you literally nothing, or something wildly different than what was advertised, they fall under the scam rule.</p>
+<br>
+<p>Also, just for the record, {{ config('app.site_name') }} will only contact you via the {{ config('app.contact') }} email address.</p>
+<p>I will also never ask for your password or other account information.</p>
+
+<br>
+<p>If you believe a moderation action was taken in error, you may contact me at {{ config('app.contact') }}.</p>
+
+<h3>TODO: explain the rest of the rules</h3>
