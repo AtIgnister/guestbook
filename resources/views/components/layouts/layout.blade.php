c0b2c0c7d0ef05607a21692acdbc5b3524119e73
Tim <137562253+AtIgnister@users.noreply.github.com>
2026-01-21 16:11:51 +0100

    screw it, just echo the whole thing

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Guestbooks')</title>
     @vite('resources/css/app.css')
-    <script src="{{ config('analytics_src') }}"></script>
+    {!! config('analytics_src') !!}
 </head>
 <body class="flex flex-col h-screen justify-between">
 
