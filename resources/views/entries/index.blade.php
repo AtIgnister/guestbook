19ac205387b8e00d2396fb0be4ca494fc139a9e4
Tim <137562253+AtIgnister@users.noreply.github.com>
2026-01-28 15:45:25 +0100

    add some css classes for custom css and remove onsave redirect

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -15,27 +15,27 @@
         <a href="/guestbooks">Back to Guestbooks</a>
     @endif
 
-    <div class="max-w-2xl mx-auto p-4 md:p-0">
+    <div class="comment-container max-w-2xl mx-auto p-4 md:p-0">
         
         @if($guestbook->description)
-            <p class="whitespace-pre-line">{{$guestbook->description}}</p>
+            <p class="guestbook-description whitespace-pre-line">{{$guestbook->description}}</p>
         @endif
 
-        <sub>Be reasonable, follow the <a href="/blog/tos">guestbook guidelines.</a></sub><br>
-        <sub>Use of this service is subject to our <a href="{{ route('privacy-policy.index') }}">privacy policy.</a></sub>
+        <sub class="guestbook-info">Be reasonable, follow the <a href="/blog/tos">guestbook guidelines.</a></sub><br>
+        <sub class="guestbook-info">Use of this service is subject to our <a href="{{ route('privacy-policy.index') }}">privacy policy.</a></sub>
 
         @if ($entries && $entries->count())
             <div class="my-10 border-solid border-2 rounded-xl p-2">
                 @if (!$is_embed)
-                    <a class="mt-3" href="{{ route('entries.create', compact('guestbook')) }}">Leave a comment!</a>
+                    <a class="comment-link mt-3" href="{{ route('entries.create', compact('guestbook')) }}">Leave a comment!</a>
                 @else
-                    <a class="mt-3" href="{{ route('embed.entries.create', compact('guestbook')) }}">Leave a comment!</a>
+                    <a class="comment-link mt-3" href="{{ route('embed.entries.create', compact('guestbook')) }}">Leave a comment!</a>
                 @endif
             </div>
         @endif
 
         @forelse ($entries as $entry)
-        <div class="my-10 border-solid border-2 rounded-xl p-2">
+        <div class="guestbook-entry my-10 border-solid border-2 rounded-xl p-2">
                 <p>{{ $entry->name }} wrote...</p>
                 @if (filter_var($entry->website, FILTER_VALIDATE_URL))
                     <sup>Website: <a href="{{ $entry->website }}">{{ $entry->website }}</a></sup>
@@ -43,11 +43,11 @@
                 <p>{{ nl2br($entry->comment) }}</p>
         </div>
         @empty
-            <p class="text-gray-500">No entries yet.</p>
+            <p class="comment-link text-gray-500">No entries yet.</p>
             @if (!$is_embed)
-                <a class="mt-3" href="{{ route('entries.create', compact('guestbook')) }}">Be the first to leave a comment!</a>
+                <a class="comment-link mt-3" href="{{ route('entries.create', compact('guestbook')) }}">Be the first to leave a comment!</a>
             @else
-                <a class="mt-3" href="{{ route('embed.entries.create', compact('guestbook')) }}">Be the first to leave a comment!</a>
+                <a class="comment-link mt-3" href="{{ route('embed.entries.create', compact('guestbook')) }}">Be the first to leave a comment!</a>
             @endif
         @endforelse
     </div>
