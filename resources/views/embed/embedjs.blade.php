2ce689fe64dc22b3337a3b969c1da5f04d52ddc9
Tim <137562253+AtIgnister@users.noreply.github.com>
2026-03-06 15:22:07 +0100

    remove all empty lines to play nice with bearblog

--- resources/views/embed/embedjs.blade.php
+++ resources/views/embed/embedjs.blade.php
@@ -4,8 +4,7 @@
     <button class="guestbooks__captcha-switch">Switch Captcha Type</button>
     <form id="guestbooks___guestbook-form" 
         action="" 
-        method="post"
-    >        
+        method="post">
         <div class="guestbooks___input-container">
             <input type="text" 
                     id="name" 
@@ -18,11 +17,8 @@
                 name="website" 
                 placeholder="website (optional)">
             </div>
-
         </div>
-
         <div id="guestbooks___challenge-answer-container"></div>
-            
         <div class="guestbooks___input-container">
             <textarea id="text" 
                 name="comment" 
@@ -34,17 +30,14 @@
             <p>Be reasonable, follow the <a target="_blank" href="{{ config('app.url') }}/blog/tos">guestbook guidelines.</a></p>
             <p>Use of this service is subject to our <a target="_blank" href="{{ config("app.url") }}/privacy-policy">privacy policy.</a></p>
         </div>
-                
         <button type="submit">submit</button>
         <div id="guestbooks___error-message"></div>
     </form>
 </div>
-
 <!-- Attribution (optional but appreciated!) -->
 <div id="guestbooks___guestbook-made-with" style="text-align: right; margin-top: 10px;">
 <small>powered by <a href="{{ config("app.url") }}" target="_blank">guestbooks</a></small>
 </div>
-
 <!-- Messages Section -->
 <hr>
 <h3 id="guestbooks___guestbook-messages-header">messages</h3>
